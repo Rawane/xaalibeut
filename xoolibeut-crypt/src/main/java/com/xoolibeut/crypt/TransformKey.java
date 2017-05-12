aa6994424adf870b1914006dedfd901e0358532a
rawane <rawane@debian-rawane.xoolibeut.com>
2017-05-12 21:38:10 +0200

    finalisation transformation key

--- xoolibeut-crypt/src/main/java/com/xoolibeut/crypt/TransformKey.java
+++ xoolibeut-crypt/src/main/java/com/xoolibeut/crypt/TransformKey.java
@@ -6,32 +6,15 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class TransformKey {
-	private static final String ALPHA = "~ABCDEFGHIJKLMNOPQRSTUVWXY!Z9812375640";
+	private static final Logger LOGGER = LoggerFactory.getLogger(TransformKey.class);
+	private static final String ALPHA = "wazbcfeskolmpkdlnsklyeABMBWcsmsddjbzrezywlUXrHoCUDEFwjGzI9/OVtRkBOSYf1VTAJCtwTON0rsNXlQ0pF5tw3yueu/xjK3NFcocOAH7PR9pdmBjGjGjUxz3AOCE4I5BXFCdx1SzqrZqh70ymMxo3mawzPXAICs5Bmtmm4vI7PN+ptCr7mkr083prcbtUiuFM8nUqQIDAQABAoICAAhdp89L2QEG2~ABCDEFGHIJKLMNOPQRSTUVWXY!Z9812375640-_^çà@)]=}+";
 
-	public static int getOrdre(String key) {
-
-		return ALPHA.indexOf(key);
-	}
-
-	public static void main(String[] args) {
-
-		try {
-			System.out.println("test equals keys "
-					+ new String(Files.readAllBytes(Paths.get("/home/rawane/devs/tmp/key/PrivateKeyM.pem"))).equals(
-							transformFile("azazzazaza", "/home/rawane/devs/tmp/key/PrivateKeyM_T_1.pem",
-									"/home/rawane/devs/tmp/key/PrivateKeyM_T_2.pem")));
-			RSAPrivateKeyUtility.convertToRSAPrivateKey(
-					new String(Files.readAllBytes(Paths.get("/home/rawane/devs/tmp/key/PrivateKeyM.pem"))));
-			System.out.println(transformFile("RawaneNdeyeAidaSalimata", "/home/rawane/devs/tmp/key/PrivateKeyM_T_1.pem",
-					"/home/rawane/devs/tmp/key/PrivateKeyM_T_2.pem"));
-			RSAPrivateKeyUtility.convertToRSAPrivateKey(transformFile("RawaneNdeyeAidaSalimata",
-					"/home/rawane/devs/tmp/key/PrivateKeyM_T_1.pem", "/home/rawane/devs/tmp/key/PrivateKeyM_T_2.pem"));
-			// RSAPrivateKeyUtility.convertToRSAPrivateKey(new
-			// String(Files.readAllBytes(Paths.get("/home/rawane/devs/tmp/key/PrivateKeyM.pem"))));
-		} catch (Exception exception) {
-			exception.printStackTrace();
-		}
+	public static int getOrdre(String key, String chaine) {
+		return chaine.indexOf(key);
 	}
 
 	/**
@@ -42,7 +25,7 @@ public class TransformKey {
 	 */
 	public static String transformLine(String line, String key) {
 		// System.out.println(line);
-		int ordre = getOrdre(key.toUpperCase());
+		int ordre = getOrdre(key.toUpperCase(), ALPHA.substring(ALPHA.indexOf("~")));
 		if (ordre > 0 && ordre < line.length()) {
 			String replace = line.substring(line.length() - 1);
 			return line.substring(0, ordre - 1) + replace + line.substring(ordre - 1, line.length() - 1);
@@ -51,6 +34,7 @@ public class TransformKey {
 	}
 
 	/**
+	 * Transformer pluseurs fichier pem en seule clé privé.
 	 * 
 	 * @param line
 	 * @param key
@@ -89,12 +73,9 @@ public class TransformKey {
 
 			return transformBuilder.toString();
 		} catch (IOException ioException) {
-
-			ioException.printStackTrace();
+			LOGGER.error("erreur", ioException);
 		}
 
 		return null;
 	}
-
-
 }
